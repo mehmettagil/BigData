32525b99adef365d4045b3cbb0a1a1fae0ea595c
mehmettagil <fuisme745@gmail.com>
2025-04-30 06:26:36 +0300

    ilk soru çözüldü

--- sql/sql_1_soru.sql
+++ sql/sql_1_soru.sql
@@ -1,10 +1,8 @@
-select SUM(capacity) AS total_capacity from ⁠ bigquery-public-data.san_francisco_bikeshare.bikeshare_station_info ⁠
 
-
-// join ile tabloları birleştirmek için
+// 1.soru 5000 den az kapasiteli bölgeleri bulmak için 
 SELECT regions.name, SUM(info.capacity) AS total_capacity
 FROM `bigquery-public-data.san_francisco_bikeshare.bikeshare_station_info` AS info
 LEFT JOIN `bigquery-public-data.san_francisco_bikeshare.bikeshare_regions` AS regions
 ON info.region_id = regions.region_id
 GROUP BY regions.name
-
+HAVING SUM(info.capacity)<5000
